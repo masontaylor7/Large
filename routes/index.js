0ab98a1e768afdc81a6cda2a450dbb77f783eab9
David Alliger <alligerdavidj@gmail.com>
2022-01-19 15:04:11 -0600

    Worked on home page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+  const isLoggedIn = res.locals.verified;
+  if (isLoggedIn) {
+
+  } else {
+    const posts =
+  }
+  res.render('index', {
+    title: 'Large Home Page',
+    isLoggedIn
+  });
 });
 
 module.exports = router;
