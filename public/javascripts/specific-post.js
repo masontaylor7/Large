8e68803b5c9f8d5c6d7b6dc15b48029d12141f0d
masontaylor7 <masonevantaylor@gmail.com>
2022-01-20 16:26:09 -0800

    added comment bar to post, began creating fetch for comments

--- public/javascripts/specific-post.js
+++ public/javascripts/specific-post.js
@@ -1,8 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event)=>{
     console.log("hello from javascript!")
     const commentButton = document.querySelector('.comment-button');
-    commentButton.addEventListener("click", (event) => {
-        console.log("Does this work?");
+    commentButton.addEventListener("click", async (event) => {
+        // console.log("Does this work?");
+        const id = window.location.pathname
+        console.log(window.location.pathname)
+        const res = await fetch(`${id}/comments`);
+        const comments = await res.json()
+        console.log(comments)
         const commentsSidebar = document.createElement('div');
         commentsSidebar.classList.add('comments-sidebar');
         const totalBody = document.querySelector('.total-body');
@@ -10,14 +15,16 @@ window.addEventListener("DOMContentLoaded", (event)=>{
         totalBody.append(commentsSidebar);
         changeOpacity.style.opacity = 0.5;
         commentsSidebar.innerHTML = `
-        <div>
-            <form action="/comments" method="post">
-                <textarea placeholder="Got something to say???"></textarea>
-                <button>Comment</button>
-            </form>
-        </div>
-        <div>
-            <img src='../imgs/x-image.png' class='x-image'>
+        <div class="upper-comment-section">
+            <div class="form-box">
+                <form action="/comments" method="post">
+                    <textarea placeholder="Got something to say???" class="comment-text-area"></textarea>
+                    <button class="comment-submit-button">Comment</button>
+                </form>
+            </div>
+            <div>
+                <img src='../imgs/x-image.png' class='x-image'>
+            </div>
         </div>
         `;
 
